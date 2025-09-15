be5a64d3b2f812193f38bfacb4817fe321b2473b
jpcodes44 <jp.mak44@gmail.com>
2025-09-15 19:43:03 -0400

    did q1

---
+++ .ipynb_checkpoints/var_docs.py
@@ -0,0 +1,18 @@
+# Units: tons
+VAR_DOCS = {
+    # outputs
+    "s_SS": "Tons of finished Stainless steel (alloy A) produced",
+    "s_HSLA": "Tons of finished High-Strength Low-Alloy (alloy B) produced",
+    # inputs allocated to Stainless (A)
+    "x_Pig_SS": "Tons of Pig iron blended into Stainless",
+    "x_Scrap_SS": "Tons of Scrap steel blended into Stainless",
+    "x_Wrought_SS": "Tons of Wrought iron blended into Stainless",
+    "x_FerroCr_SS": "Tons of Ferrochromium blended into Stainless",
+    "x_FerroNi_SS": "Tons of Ferronickel blended into Stainless",
+    # inputs allocated to HSLA (B)
+    "x_Pig_HSLA": "Tons of Pig iron blended into HSLA",
+    "x_Scrap_HSLA": "Tons of Scrap steel blended into HSLA",
+    "x_Wrought_HSLA": "Tons of Wrought iron blended into HSLA",
+    "x_FerroCr_HSLA": "Tons of Ferrochromium blended into HSLA",
+    "x_FerroNi_HSLA": "Tons of Ferronickel blended into HSLA",
+}
